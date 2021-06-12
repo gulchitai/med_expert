82bd40e62f05a18880126f74f38dff64fd5e85ff
gulchitai <gulchitai@bk.ru>
2021-06-12 21:30:44 +1000

    парсер актуальных стандаров структурированный

--- consultant_plus_parser.py
+++ consultant_plus_parser.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 
 import requests
 from lxml import html
+import pandas as pd
 from pymongo import MongoClient
 
 #некоммерческая версия консультант плюс
@@ -51,14 +52,36 @@ if __name__ == "__main__":
     print(info)
 
     main_link = 'http://www.consultant.ru'
+    l = []
     for link in info:
         endpoint = main_link + link
         response = requests.get(url=endpoint, headers=headers).text
         root = html.fromstring(response)
+        title = ''
         for i in root.xpath('//tr'):
-            t = i.xpath("./td[1]/div/div/span/text()")
-            #t = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-            print(t)
+            t0 = i.xpath("./td[1]/div/h1/div/span/span/text()")
+            t1 = i.xpath("./td[1]/div/div/span/text()")
+            t2 = i.xpath("./td[2]/div/div/span/text()")
+            t3 = i.xpath("./td[3]/div/div/span/text()")
+            t4 = i.xpath("./td[4]/div/div/span/text()")
+            if len(t0) > 0:
+                title = t0[0]
 
+            if (t1 == []) and (t2 == []) and (t3 == []) and (t4 == []):
+                continue
+            d = {}
+            d['title'] = title
+            d['name'] = t1[0]
+            d['mkb'] = t2
+            d['age'] = t3
+            d['doc_name'] = t4
+            l.append(d)
+
+    print(l)
+
+    client = MongoClient('localhost', 27017)
+    db = client['med_expert']
+    db.drop_collection('standart')
+    db['standart'].insert_many(l)
 
     #download_document(headless=False, name="Приказ Минздрава России от 20 декабря 2012 г. N 1139н")
