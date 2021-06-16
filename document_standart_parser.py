a6cc46c368b3f4ae4f1a31c6ecf5db68ad1a38c7
gulchitai <gulchitai@bk.ru>
2021-06-16 14:42:59 +1000

    распарсила первую таблицу  1 стандарта

--- document_standart_parser.py
+++ document_standart_parser.py
@@ -4,20 +4,43 @@ from pprint import pprint
 import re
 from pymongo import MongoClient
 
-def get_standart_data():
+def get_number_date(filename):
+    match = re.search(r'(\d+.\d+.\d+)', filename)
+    date = match.group(1)
+    lst = filename.split(sep=" ")
+    flag = False
+    num = ''
+    for s in lst:
+        if s == 'N':
+            flag = True
+            continue
+        if flag:
+            num = s
+            break
+    return num, date
+
+def get_standart_table1():
 
     filename = './docs/Приказ Минздрава России от 20.12.2012 N 1095н  Об утверждени.htm'
+
     f = open(filename, 'r', encoding="utf8")
     result = f.read()
     parsed_html = bs(result, 'lxml')
     tables = parsed_html.find_all("table")
 
-    l = []
-    for table in tables:
-        d = {}
+    for k, table in enumerate(tables):
+        if k > 2:
+            break
+        if k == 0:
+            d = {}
+            rl = []
+            num, date = get_number_date(filename)
+            d['date'] = date
+            d['number'] = num
+            d['table_name'] = table.find_previous_sibling('div').find_previous_sibling('div').get_text()
 
         rows = table.find_all("tr")
-        rl = []
+
         for i, row in enumerate(rows):
             if i < 2:
                 continue
@@ -25,19 +48,27 @@ def get_standart_data():
             dc = {}
             for j, col in enumerate(columns):
                 if j == 0:
-                    dc['code'] = col.get_text()
+                    dc['code'] = ' '.join(col.get_text().split())
                 if j == 1:
-                    dc['name'] = col.get_text()
+                    dc['name'] = ' '.join(col.get_text().split())
                 if j == 2:
-                    dc['chast'] = col.get_text()
+                    dc['chast'] = ' '.join(col.get_text().split())
                 if j == 3:
-                    dc['krat'] = col.get_text()
+                    dc['krat'] = ' '.join(col.get_text().split())
             rl.append(dc)
-        d['table_name'] = table.find_previous_sibling('div').find_previous_sibling('div').get_text()
-        d['rows'] = rl
-        l.append(d)
-    return l
+        if k == 2:
+            d['rows'] = rl
+    return d
+
+
 
 if __name__ == "__main__":
 
-    pprint(get_standart_data())
+    path = ".\docs\Стандарты"
+
+    client = MongoClient('localhost', 27017)
+    db = client['med_expert']
+    db.drop_collection('standart_table1')
+
+    data = get_standart_table1()
+    db['standart_table1'].insert_one(data)
